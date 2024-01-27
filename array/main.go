ada5673c70941c0507128460a092e38b62cf8c49
hisamatsu yoshiyuki <hisasann25@gmail.com>
2024-01-27 14:14:58 +0900

    feat: add array/main.go

---
+++ array/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{1, 2, 3}
+	fmt.Println(a)
+	fmt.Println(a[0])
+	fmt.Println(a[1])
+	fmt.Println(a[2])
+
+	a[0] = 0
+	fmt.Println(a[0])
+
+	// 要素数を省略できる
+	b := [...]string{"A", "B", "C"}
+	fmt.Println(b)
+
+	c := []string{"AA", "BB", "CC"}
+	fmt.Println(c)
+}
