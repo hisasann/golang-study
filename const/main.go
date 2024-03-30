c2d4dad219b51b5f7cfddb4ac5d6b2514b0359f5
hisamatsu yoshiyuki <hisasann25@gmail.com>
2024-03-31 01:50:38 +0900

    feat: add constants

---
+++ const/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	constants "hisasann/golang-study/pkg"
+)
+
+func main() {
+	fmt.Println(constants.Hoge)
+}
---
+++ pkg/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+type H string
+
+const (
+	Hoge H = "hoge"
+	Foo  H = "foo"
+	Fuga H = "fuga"
+	Bar  H = "bar"
+)
